ed51400c312e9eaad78419e73d425403a80ae314
krutharthsr <97036056+krutharthsr@users.noreply.github.com>
2022-01-03 19:36:07 +0530

    Update areaofc.py

--- areaofc.py
+++ areaofc.py
@@ -1,3 +1,6 @@
+#get input radius of the circle
 r=int(input("Input the radius of the circle:"))
+#calculate the area of the circle
 a=3.14159*r**2
+#diplay the area of the circle
 print("The area of the circle with radius",r,"is",a)
