4f19ee0b327dda53694611b3bf51a82f940d2faf
krutharthsr <97036056+krutharthsr@users.noreply.github.com>
2022-01-23 13:56:34 +0530

    Update areaofc.py

--- areaofc.py
+++ areaofc.py
@@ -7,18 +7,4 @@ a=3.14159*r**2
 print("The area of the circle with radius",r,"is",a)
 
 
-#extension
-import os
-
-
-# this will return a tuple of root and extension
-split_tup = os.path.splitext('my_file.txt')
-print(split_tup)
-
-# extract the file name and extension
-file_name = split_tup[0]
-file_extension = split_tup[1]
-
-print("Input the Filename: ", file_name)
-print("The File extension of the file is: ", file_extension)
 
