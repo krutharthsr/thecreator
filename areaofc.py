ea5d46b3171a66b6f729f376eae004faec020bd0
krutharthsr <97036056+krutharthsr@users.noreply.github.com>
2022-01-23 13:48:22 +0530

    Update areaofc.py

--- areaofc.py
+++ areaofc.py
@@ -1,6 +1,24 @@
+#circle
 #get input radius of the circle
 r=int(input("Input the radius of the circle:"))
 #calculate the area of the circle
 a=3.14159*r**2
 #diplay the area of the circle
 print("The area of the circle with radius",r,"is",a)
+
+
+#extension
+import os
+
+
+# this will return a tuple of root and extension
+split_tup = os.path.splitext('my_file.txt')
+print(split_tup)
+
+# extract the file name and extension
+file_name = split_tup[0]
+file_extension = split_tup[1]
+
+print("Input the Filename: ", file_name)
+print("The File extension of the file is: ", file_extension)
+
